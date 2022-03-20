03f14ba35fa1759e22a32e352daaf0b9d04c3779
Yu-Wei Chang <andy.yu29@gmail.com>
2022-03-20 19:05:50 +0800

    Add cmake method and change Kinova IP address

--- api_cpp/examples/scripts/build-gcc.sh
+++ api_cpp/examples/scripts/build-gcc.sh
@@ -10,8 +10,11 @@ build_folder="build-gcc-${build_type}"
 
 kortex_api_folder="$2"
 
+# Build files under the folder which you want
+target_folder="108-Gen3_torque_control"
+
 mkdir -p ${build_folder}
 cd ${build_folder}
 rm -rf *
 
-cmake .. -DCMAKE_BUILD_TYPE=${build_type} -DKORTEX_SUB_DIR=${kortex_api_folder} && make
+cmake .. -DUSE_CONAN=OFF -DCMAKE_BUILD_TYPE=${build_type} -DKORTEX_SUB_DIR=${kortex_api_folder} -DTARGET_DIR=${target_folder} && make
--- api_cpp/examples/utilities.cpp
+++ api_cpp/examples/utilities.cpp
@@ -7,7 +7,7 @@ ExampleArgs ParseExampleArguments(int argc, char *argv[])
     cxxopts::Options options(argv[0], "Kortex example");
     
     options.add_options()
-        ("ip", "IP address of destination", cxxopts::value<std::string>()->default_value("192.168.1.10"))
+        ("ip", "IP address of destination", cxxopts::value<std::string>()->default_value("192.168.50.131"))
         ("h,help", "Print help")
         ("u,username", "username to login", cxxopts::value<std::string>()->default_value("admin"))
         ("p,password", "password to login", cxxopts::value<std::string>()->default_value("admin"))
